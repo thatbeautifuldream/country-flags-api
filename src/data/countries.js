a9448054e7ec99c5d1d03275c87c1a296affaeff
Milind Mishra <milind.mishra4@gmail.com>
2024-12-13 16:38:48 +0530

    Add countries data module with country names for flag service

---
+++ src/data/countries.js
@@ -0,0 +1,15 @@
+const countries = {
+  US: "United States",
+  GB: "United Kingdom",
+  FR: "France",
+  DE: "Germany",
+  IT: "Italy",
+  ES: "Spain",
+  PT: "Portugal",
+  NL: "Netherlands",
+  BE: "Belgium",
+  CH: "Switzerland",
+  // Add more countries as needed...
+};
+
+module.exports = countries;
